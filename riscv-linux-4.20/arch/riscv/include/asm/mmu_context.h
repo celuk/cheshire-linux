70a17d7111240646e92f1ba07ba69092fcbccacb
celuk <seyyid4091@gmail.com>
2026-01-07 19:53:53 +0300

    fix compilation errors because of compiler version

--- riscv-linux-4.20/arch/riscv/include/asm/mmu_context.h
+++ riscv-linux-4.20/arch/riscv/include/asm/mmu_context.h
@@ -88,7 +88,7 @@ static inline void switch_mm(struct mm_struct *prev,
 		 * name to support binutils 2.29 which doesn't know about the
 		 * privileged ISA 1.10 yet.
 		 */
-		csr_write(sptbr, virt_to_pfn(next->pgd) | SATP_MODE);
+		csr_write(satp, virt_to_pfn(next->pgd) | SATP_MODE);
 		local_flush_tlb_all();
 
 		flush_icache_deferred(next);
