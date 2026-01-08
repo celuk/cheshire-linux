47470228781c9a22e8752339142a7fa743c59d8c
celuk <seyyid4091@gmail.com>
2026-01-08 06:08:41 +0300

    add lrzsz compiling script

---
+++ lrzsz/compile.sh
@@ -0,0 +1,9 @@
+set -e
+
+autoconf
+CC=/media/shc/0EDEBC4906059163/tools/riscv-toolchain-linux/_install/bin/riscv64-unknown-linux-gnu-gcc CXX=/media/shc/0EDEBC4906059163/tools/riscv-toolchain-linux/_install/bin/riscv64-unknown-linux-gnu-g++ ./configure --host=riscv64
+make
+cp src/lsz .
+cp src/lrz .
+cp lrz rz
+cp lsz sz
---
+++ lrzsz/version.go
@@ -0,0 +1,9 @@
+package main
+
+var Package string = "lrzsz" // lrzsz
+var PackageBugreport string = "<uwe@ohse.de>" // uwe@ohse.de
+var PackageName string = "GNU lrzsz" // GNU lrzsz
+var PackageVersion string = "0.13.0-alpha" // lrzsz-0.13.0-alpha
+var PackageURL string = "https://ohse.de/uwe/software/lrzsz.html" // https://ohse.de and so on
+var Build string // the linker sets this from git head
+
