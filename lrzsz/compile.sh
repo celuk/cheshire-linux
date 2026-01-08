14cca51ee75cf55b44a33ed0acde8e0ddfdff9c1
celuk <seyyid4091@gmail.com>
2026-01-08 06:48:18 +0300

    statically link lrsz

--- lrzsz/compile.sh
+++ lrzsz/compile.sh
@@ -1,8 +1,8 @@
 set -e
 
 autoconf
-CC=/media/shc/0EDEBC4906059163/tools/riscv-toolchain-linux/_install/bin/riscv64-unknown-linux-gnu-gcc CXX=/media/shc/0EDEBC4906059163/tools/riscv-toolchain-linux/_install/bin/riscv64-unknown-linux-gnu-g++ ./configure --host=riscv64
-make
+CC=/media/shc/0EDEBC4906059163/tools/riscv-toolchain-linux/_install/bin/riscv64-unknown-linux-gnu-gcc CXX=/media/shc/0EDEBC4906059163/tools/riscv-toolchain-linux/_install/bin/riscv64-unknown-linux-gnu-g++ LDFLAGS="-static" ./configure --host=riscv64
+make LDFLAGS="-all-static"
 cp src/lsz .
 cp src/lrz .
 cp lrz rz
